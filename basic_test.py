e7fdcba19955720c05dfc81b9671d7d8bbe50055
PaidupatiBharath <37590566+PaidupatiBharath@users.noreply.github.com>
2025-07-19 22:41:57 +0530

    Add files via upload

---
+++ basic_test.py
@@ -0,0 +1,14 @@
+def add_numbers(a, b):
+    """Function to add two numbers.
+    Args:
+        a (int): The first number.
+        b (int): The second number.
+    Returns:
+        int: The sum of the two numbers.
+    """
+    return a + b
+
+a= 10
+b = 20
+c = add_numbers(a, b)
+print(c)
